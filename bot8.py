27d34c20c64c5f30add54c1e567ad5b6d41b288b
Tatiana Kuzakhmetova <tkuzahmetova@gmail.com>
2019-11-11 17:31:42 +0300

    Revert "New README file"
    This reverts commit 4289b58480eb688c10a06ffec62f84f11d0cef8e.
    
    	modified:   README.rst
    	deleted:    __pycache__/bot.cpython-37.pyc
    	deleted:    __pycache__/handlers.cpython-37.pyc
    	deleted:    __pycache__/settings.cpython-37.pyc
    	deleted:    __pycache__/utils.cpython-37.pyc
    	deleted:    bot10.py
    	deleted:    bot2.py
    	deleted:    bot3.py
    	deleted:    bot4.py
    	deleted:    bot5.py
    	deleted:    bot6.py
    	deleted:    bot7.py
    	deleted:    bot8.py
    	deleted:    bot9.py
    	deleted:    new 1

--- bot8.py
+++
@@ -1,6 +0,0 @@
-import requests
-
-proxies = {'http': 'socks5://t3.learn.python.py:1080'}
-
-r = requests.get("https://api.telegram.org/bot964358666:AAFzc4rqY38z9-QnUyGXH27F5oy7a1CpzXo/getMe", proxies = proxies)
-print (r.text)
